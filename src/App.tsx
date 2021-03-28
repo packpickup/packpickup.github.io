0c47566c0a927beaeb99487e8da7b8305aec2daf
Aamir Afridi 😎 <aamir.afridi@bourne-leisure.co.uk>
2021-03-28 00:18:51 +0000

    testing ajax data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 
-import { FunctionComponent } from "react";
+import { FunctionComponent, useEffect } from "react";
 import { HashRouter, Route, Switch } from "react-router-dom";
 
 import { Header } from "./components/Header";
@@ -8,6 +8,12 @@ import { Detail } from "./pages/Detail";
 import { Home } from "./pages/Home";
 
 export const App: FunctionComponent = () => {
+  useEffect(() => {
+    fetch("https://packpickup.com/data.json")
+      .then(console.log)
+      .catch(console.error);
+  }, []);
+
   return (
     <HashRouter basename="/">
       <Header />
