69a88f63d082c6dab16af1623ac3d89cddaa91ca
Aamir Afridi 😎 <aamirafridi@gmail.com>
2022-03-26 23:48:15 +0000

    fixing pwa

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import { App } from "./App";
-import * as serviceWorker from "./serviceWorker";
-// import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -17,8 +16,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-serviceWorker.register();
+serviceWorkerRegistration.register();
