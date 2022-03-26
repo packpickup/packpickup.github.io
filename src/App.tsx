d9ac164f6bca7003fba41d57d32214839db20de3
Aamir Afridi 😎 <aamirafridi@gmail.com>
2022-03-26 23:15:43 +0000

    trying PWA

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 
+import AddToHomeScreen from "@ideasio/add-to-homescreen-react";
 import { FunctionComponent } from "react";
 import { Container } from "react-bootstrap";
 import { HashRouter, Route, Switch } from "react-router-dom";
@@ -10,24 +11,26 @@ import { SearchResults } from "./pages/SearchResults";
 
 export const App: FunctionComponent = () => {
   return (
-    <HashRouter basename="/">
-      <Container style={{ maxWidth: 540 }}>
-        <Header />
-        <div className="content">
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/search-results">
-              <SearchResults />
-            </Route>
-          </Switch>
-        </div>
-        <footer className="footer">
-          <small>© PackPickup {new Date().getFullYear()}</small>
-          <h6>------------</h6>
-        </footer>
-      </Container>
-    </HashRouter>
+    <>
+      <AddToHomeScreen />
+      <HashRouter basename="/">
+        <Container style={{ maxWidth: 540 }}>
+          <Header />
+          <div className="content">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/search-results">
+                <SearchResults />
+              </Route>
+            </Switch>
+          </div>
+          <footer className="footer">
+            <small>© PackPickup {new Date().getFullYear()}</small>
+          </footer>
+        </Container>
+      </HashRouter>
+    </>
   );
 };
