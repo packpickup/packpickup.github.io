b5d29b5ab36d48bb86ac208e36b1d5421b1910f9
Aamir Afridi 😎 <aamirafridi@gmail.com>
2022-03-26 21:52:10 +0000

    fixed the formating

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import { App } from "./App";
-import reportWebVitals from "./reportWebVitals";
+import * as serviceWorker from "./serviceWorker";
+// import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -19,4 +20,5 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
+serviceWorker.register();
